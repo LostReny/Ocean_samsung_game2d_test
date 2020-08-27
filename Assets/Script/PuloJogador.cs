63ffc46c9ba109f4517e5b892dd8bed34df8e8a1
LostReny <68451719+LostReny@users.noreply.github.com>
2020-08-27 16:35:09 -0300

    implementação pulo

--- Assets/Script/PuloJogador.cs
+++ Assets/Script/PuloJogador.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PuloJogador : MonoBehaviour
 {
+
+    public Rigidbody2D rb;
+
+    public float forcaPulo;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+            rb.AddForce(Vector2.up * forcaPulo);
+        }
     }
 }
